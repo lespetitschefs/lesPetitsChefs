289cabd3f93b0dc77e81f849cf037e20d3a075ac
fbremec <fbremec@gmail.com>
2017-11-25 23:33:22 +0100

    refactoring with firebase

--- recettes.js
+++ recettes.js
@@ -1,4 +1,4 @@
-var jsonRecettesApperitifs = {};
+/*var jsonRecettesApperitifs = {};
 jsonRecettesApperitifs["other test"] = ({"nom":"other test","type":"","image":"","difficulte":5,"prepa":"30 min","cuisson":"15 min","nbPersonne":2,"prix":"10 euro"});
 jsonRecettesApperitifs["test"] = ({"nom":"test","type":"","image":"","difficulte":5,"prepa":"30 min","cuisson":"15 min","nbPersonne":2,"prix":"10 euro"});
 
@@ -15,6 +15,52 @@ jsonRecettesDesserts["Choco cake"] = ({"nom":"Choco cake","type":"Gâteau","imag
 var jsonRecettesPlats = {};
 jsonRecettesPlats["Moussaka grecque"] = ({"nom":"Moussaka grecque","type":"Gratin","image":"https://www.cuisine-etudiant.fr/image/recette/800500/b5d2d693e48c115c8c96ef7bf214de6f5fa25f54-moussaka-grecque-traditionnelle.jpg","difficulte":1,"prepa":"45 min","cuisson":"45 min","nbPersonne":4,"prix":"5 euro",
 										"ingredients":{
-											"":["500g de viande de boeuf hachée à 5% MG","4 aubergine","600g de tomates","2 oignons","4 cubes debouillon de volaille dégraissé","40g de fécule de mais","500ml de lait écrémé (demi-écrémé à 1600kcal et à 1800kcal","4 pincée de cumin","4 pincée de cannelle","4 pincée de noix de muscade"]},
+											"Moussaka":["500g de viande de boeuf hachée à 5% MG","4 aubergine","600g de tomates","2 oignons","4 cubes debouillon de volaille dégraissé","40g de fécule de mais","500ml de lait écrémé (demi-écrémé à 1600kcal et à 1800kcal","4 pincée de cumin","4 pincée de cannelle","4 pincée de noix de muscade"]},
 										"preparation":{
-											"":["Laver les légumes","Couper l'aubergine en fines tranches dans la longeur","parsemer de sel et laisser dégorger 30 minutes","Dans une poele à revetement antiadhésif, faire griller chaque tranche à sec.Réserver","Dans la meme poele, faire revenir à sec l'oigon, ajouter la viande de boeuf hachée, le cumin, la cannelle.","Ajouter les dés de tomates, ainsi que les cube de bouillon de volaille dégraissé.","AJouter un peu d'eau si les tomates n'en rendent pas suffisamment.","Préparer la béchamel diététique : délayer la fécule de mais avec un peu de lait froid.","Porter de reste de lait à ébullition, puis verser sur le premier mélange.","Remettre le tout dans la casserole et porter sur le feu jusqu'a épaississement, sans cesser de remuer","Assaisonner de sel, poivre, et muscade rapée.","Préchauffer le four à 180°C (th. 6).","Dans un plat à gratin, alterner les tranches d'aubergines et la viande en sauce.","Terminer par la couche de béchamel diététique.","Enfourner 45 minutes, servir chaud."]}});
+											"Moussaka":["Laver les légumes","Couper l'aubergine en fines tranches dans la longeur","parsemer de sel et laisser dégorger 30 minutes","Dans une poele à revetement antiadhésif, faire griller chaque tranche à sec.Réserver","Dans la meme poele, faire revenir à sec l'oigon, ajouter la viande de boeuf hachée, le cumin, la cannelle.","Ajouter les dés de tomates, ainsi que les cube de bouillon de volaille dégraissé.","AJouter un peu d'eau si les tomates n'en rendent pas suffisamment.","Préparer la béchamel diététique : délayer la fécule de mais avec un peu de lait froid.","Porter de reste de lait à ébullition, puis verser sur le premier mélange.","Remettre le tout dans la casserole et porter sur le feu jusqu'a épaississement, sans cesser de remuer","Assaisonner de sel, poivre, et muscade rapée.","Préchauffer le four à 180°C (th. 6).","Dans un plat à gratin, alterner les tranches d'aubergines et la viande en sauce.","Terminer par la couche de béchamel diététique.","Enfourner 45 minutes, servir chaud."]}});
+
+
+*/
+
+
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyBjqtR73Ajz-3r31UWengqGhUyDLCeAptM",
+	authDomain: "lespetitschefs-170116.firebaseapp.com",
+	databaseURL: "https://lespetitschefs-170116.firebaseio.com",
+	projectId: "lespetitschefs-170116",
+	storageBucket: "lespetitschefs-170116.appspot.com",
+	messagingSenderId: "747566514049"
+};
+if (!firebase.apps.length) {
+	firebase.initializeApp(config);
+}
+var database = firebase.database();
+var jsonRecettesDesserts = {};
+var jsonRecettesApperitifs = {};
+var jsonRecettesPlats = {};
+
+
+
+firebase.database().ref('/desserts/').once('value').then(function(snapshot) {
+	jsonRecettesDesserts = (snapshot.val() && snapshot.val().recettes) || 'Anonymous';
+	var event = new Event('notifyDessertChanged');
+	document.getElementById("notify").dispatchEvent(event);
+});
+firebase.database().ref('/apperitifs/').once('value').then(function(snapshot) {
+	jsonRecettesApperitifs = (snapshot.val() && snapshot.val().recettes) || 'Anonymous';
+	var event = new Event('notifyApperitifsChanged');
+	document.getElementById("notify").dispatchEvent(event);
+});
+firebase.database().ref('/plats/').once('value').then(function(snapshot) {
+	jsonRecettesPlats = (snapshot.val() && snapshot.val().recettes) || 'Anonymous';
+	var event = new Event('notifyPlatsChanged');
+	document.getElementById("notify").dispatchEvent(event);
+});
+/*function writeApperitifs(appertif) {
+  firebase.database().ref('desserts/').set({
+    recettes: appertif
+  });
+}*/
+
+//writeApperitifs(jsonRecettesDesserts);*/
