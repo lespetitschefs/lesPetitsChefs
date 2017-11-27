c33f60d4545aaabcf0093e45b0e06ca1637e2a9e
lespetitschefs <32219055+lespetitschefs@users.noreply.github.com>
2017-11-27 14:18:14 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -6,3 +6,11 @@ var typeSecondaire = {
 	'plats':['pate','Gratin'],
 	'desserts':['Gâteau','Flanc']
 }
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi,    
+    function(m,key,value) {
+      vars[key] = value;
+    });
+    return vars;
+  }
